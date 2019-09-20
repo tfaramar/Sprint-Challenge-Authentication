2b2e03670695c86394089098ff1837b734f3ac95
tatiana faramarzi <tfaramarzi@gmail.com>
2019-09-20 08:45:25 -0700

    answered self-study questions, created model for db interactions, implemented password hashing and verifying for register and login operations

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,41 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+//const jwt = require('jsonwebtoken');
+
+//create model for db interactions
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  //hash password
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'There was a problem registering the user.'});
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json(user);
+    } else {
+      res.status(401).json({ message: 'Please provide valid credentials.' });
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'There was a problem logging in the user.'});
+  });
 });
+
+
 
 module.exports = router;
